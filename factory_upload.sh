ff88867691eb3ffde1d4d43054557faf65af874c
Jerar <iliev.georgi@gmail.com>
2022-11-25 12:05:39 +0200

    1

--- factory_upload.sh
+++ factory_upload.sh
@@ -1,3 +1,3 @@
-avrdude -c USBasp -p m328p -U lfuse:w:0xFF:m -U hfuse:w:0xDF:m -U efuse:w:0xFD:m -B6
-avrdude -p atmega328p -B6 -c usbasp -P usb -e -U flash:w:openevse.hex
+avrdude -c USBasp -p m328p -U lfuse:w:0xFF:m -U hfuse:w:0xDF:m -U efuse:w:0xFD:m -B4
+avrdude -p atmega328p -B4 -c usbasp-clone -P usb -e -U flash:w:openevse.hex
 
